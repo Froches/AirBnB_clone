5ba7e7594856f9c9d0345e380608150ef4a94756
Froches <michaelfavour418@gmail.com>
2023-10-13 04:58:30 -0700

    BaseModel commented

--- models/base_model.py
+++ models/base_model.py
@@ -1,25 +1,30 @@
 #!/usr/bin/python3
-
+"""Base model class"""
 import uuid
 from datetime import datetime
 
-class BaseModel():
-    def __init__(self):
+
+class BaseModel:
+    """Base model class. Parent of all other subclasses"""
+
+    def __init__(self, *args, **kwargs):
+        """Initializes T=the BaseModel class"""
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
-        self.updated_at = self.created_at
+        self.updated_at = datetime.now()
 
     def __str__(self):
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
-
+        """String representation of BaseModel class"""
+        return (f"[{self.__class__.__name__}] [{self.id}] {self.__dict__}")
+    
     def save(self):
+        """Updates public instance attribute 'updated_at' with current datetime"""
         self.updated_at = datetime.now()
 
     def to_dict(self):
+        """Returns a dictionary containing all key/values of instance"""
         obj_dict = self.__dict__.copy()
-
         obj_dict['__class__'] = self.__class__.__name__
-
         obj_dict['created_at'] = self.created_at.isoformat()
         obj_dict['updated_at'] = self.updated_at.isoformat()
-
+        return obj_dict
