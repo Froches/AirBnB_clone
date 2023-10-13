e58ff0c030b4821f2ddcb2ff901172ef64d38621
Froches <michaelfavour418@gmail.com>
2023-10-13 05:17:56 -0700

    Create BaseModel from dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -9,16 +9,26 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """Initializes T=the BaseModel class"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            if '__class__' in kwargs:
+                kwargs.pop('__class__')
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    setattr(self, key, datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
+                else:
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """String representation of BaseModel class"""
-        return (f"[{self.__class__.__name__}] [{self.id}] {self.__dict__}")
+        st=(f"[{self.__class__.__name__}] [{self.id}] {self.__dict__}")
+        return st
     
     def save(self):
-        """Updates public instance attribute 'updated_at' with current datetime"""
+        """Update public inst attr 'updated_at' with current datetime"""
         self.updated_at = datetime.now()
 
     def to_dict(self):
